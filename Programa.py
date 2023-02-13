23945791c3ba6a9be4f9500d7d80dacf27fbd429
Annie Carrillo <79266800+anacarrillogalvez@users.noreply.github.com>
2023-02-13 12:32:07 -0600

    Finalizacion del programa

---
+++ Programa.py
@@ -0,0 +1,23 @@
+n1 = input("¿Dime el primer número?")
+n2 = input("¿Dime el segundo número?")
+
+print (n1+n2)
+
+
+try:
+  n1 = int(input("¿Dime el primer número?"))
+except ValueError:
+        print("El valor introducido no es un número. Intenta de nuevo")
+
+
+while True:
+    try:
+        n1 = int(input("¿Dime el primer número?"))
+        break
+    except ValueError:
+        print("El valor introducido no es un número. Intenta de nuevo")
+
+
+suma = n1+n2
+
+print ("La suma de los dos números es: " + str(n1+n2))
